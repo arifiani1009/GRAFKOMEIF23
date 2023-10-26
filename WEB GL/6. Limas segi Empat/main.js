845402fa9c72eccb73c17f44094ab36416c696b5
arifiani1009 <arifiani.tc09@gmail.com>
2023-10-26 15:35:52 +0700

    Menggambar 3D
    Menggambar Limas Segiempat

--- WEB GL/6. Limas segi Empat/main.js
+++ WEB GL/6. Limas segi Empat/main.js
@@ -3,20 +3,32 @@ function main(){
     var gl = canvas.getContext("webgl");
 
     var vertexShaderCode = `
-        attribute vec2 aPosition;
+        attribute vec3 aPosition;
         attribute vec3 aColor;
-        uniform mat4 uMatrix;
+        uniform mat4 uModel;
+        uniform mat4 uProjection;
+        uniform mat4 uView;
         varying vec3 vColor;
         void main(){
             vColor = aColor;
-            gl_Position = vec4(aPosition, 0.0, 1.0) * uMatrix;
+            gl_Position = vec4(aPosition, 1.0) * uModel * uProjection * uView;
         }
         `;
 
+    //vertex buffer
     var vertexBuffer = gl.createBuffer();
     gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
-    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
-    gl.bindBuffer(gl.ARRAY_BUFFER, null);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(triangles), gl.STATIC_DRAW);
+
+    //color buffer
+    var colorBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(colors), gl.STATIC_DRAW);
+
+    //index buffer
+    var indexBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer);
+    gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(indices), gl.STATIC_DRAW);
 
     var vertexShader = gl.createShader(gl.VERTEX_SHADER);
     gl.shaderSource(vertexShader, vertexShaderCode);
@@ -34,29 +46,65 @@ function main(){
     gl.linkProgram(program);
     gl.useProgram(program);
 
+    //menambahkan vertices ke dalam aPosition dan aColor untuk digambar
+    //position
     gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
     var aPosition = gl.getAttribLocation(program, "aPosition");
-    gl.vertexAttribPointer(aPosition, 2, gl.FLOAT, false, 5*Float32Array.BYTES_PER_ELEMENT, 0);
+    gl.vertexAttribPointer(aPosition, 3, gl.FLOAT, false, 0, 0);
     gl.enableVertexAttribArray(aPosition);
 
+    //color
+    gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
     var aColor = gl.getAttribLocation(program, "aColor");
-    gl.vertexAttribPointer(aColor, 3, gl.FLOAT, false, 5*Float32Array.BYTES_PER_ELEMENT, 2*Float32Array.BYTES_PER_ELEMENT);
+    gl.vertexAttribPointer(aColor, 3, gl.FLOAT, false, 0, 0);
     gl.enableVertexAttribArray(aColor);
-    var x = 0.0, y = 0.0, z = 0.0;
+
+    var Pmatrix = gl.getUniformLocation(program, "uProjection");
+    var Vmatrix = gl.getUniformLocation(program, "uView");
+    var Mmatrix = gl.getUniformLocation(program, "uModel");
+    gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
+    
+    var projmatrix = getprojection(45, canvas.width/canvas.height, 1, 100);
+    var modmatrix = [
+        1,0,0,0,
+        0,1,0,0,
+        0,0,1,0,
+        0,0,0,1];
+    var viewmatrix = [
+        1,0,0,0,
+        0,1,0,0,
+        0,0,1,0,
+        0,0,0,1];
+
+    viewmatrix[14] = viewmatrix[14]-3;
+    viewmatrix[13] = viewmatrix[13];
+    viewmatrix[12] = viewmatrix[12];
 
     function render(time){
         if(!freeze){
-            x = x + 0.001;
+            translasi(modmatrix,0.01,0.0,0.0);
+            rotasiX(modmatrix, 0.01);
         }
 
-        translation(x, y, z, gl, program);
+        //kedalaman (z)
+        gl.enable(gl.DEPTH_TEST);
+        gl.depthFunc(gl.LEQUAL);
 
         gl.clearColor(1.0, 1.0, 1.0, 1.0);
-        gl.clear(gl.COLOR_BUFFER_BIT);
-    
-        gl.drawArrays(gl.LINES, 0, 12);
-        window.requestAnimationFrame(render);
-    }
+        gl.clearDepth(1.0);
 
+        gl.viewport(0.0, 0.0, canvas.width, canvas.height);
+        gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+        gl.uniformMatrix4fv(Pmatrix, false, projmatrix);
+        gl.uniformMatrix4fv(Vmatrix, false, viewmatrix);
+        gl.uniformMatrix4fv(Mmatrix, false, modmatrix);
+
+        gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer);
+        gl.drawElements(gl.TRIANGLES, indices.length, gl.UNSIGNED_SHORT, 0);
+
+        window.requestAnimationFrame(render);
+    }  
+    
     render(1);
 }
